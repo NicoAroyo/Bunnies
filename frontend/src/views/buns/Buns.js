6a4e11750c7b9d5bb176c091c7d33cfaab1455d2
NicoAroyo <nico.aroyo@gmail.com>
2022-09-25 13:16:18 +0300

    AAAAAAAAAAAAAA

--- frontend/src/views/buns/Buns.js
+++ frontend/src/views/buns/Buns.js
@@ -14,8 +14,9 @@ export const Buns = () => {
    const [users , setUsers] = useState([]);
    const [rsIds , setRsIds] = useState([]);
    const navigate = useNavigate();
-   
+   console.log(user);
   useEffect(async() => {
+    
     const service = new RelationshipsService(); 
     const friendsIds = await service.getFriends(user._id);
     setRsIds(friendsIds); 
@@ -28,18 +29,18 @@ export const Buns = () => {
     const blockedUsers = service.getBlocked(user._id);
     const usersData = userService.getUsers();
     setUsers(usersData);
-    setFriends(friendsData);
+    setFriends(friendData);
   }, []);
 
   const removeFriend = async (id) => {
-    const idToDelete = rsIds.find(id === userId2);
+    const idToDelete = rsIds.find((r) => r.userId2 === id);
     const service = new RelationshipsService(); 
    await service.deleteAsync(idToDelete); 
   }
 
   const addFriend = async (id) => {
     const service = RelationshipsService(); 
-    const friendship = {userId1 : user._id, userId2 : id, type : "friends"};
+    const friendship =  new {userId1 : user._id, userId2 : id, type : "friends"};
     await service.postAsync(friendship);
   }
 
@@ -56,7 +57,7 @@ export const Buns = () => {
           </div>
         );
       })}
-      {users?.mapp((user) => {
+      {users?.filter((item) => !friends.includes(item._id)).map((user) => {
         return(
           <div>
             <h3>{user.firstName}</h3>
